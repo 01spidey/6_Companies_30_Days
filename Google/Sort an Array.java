4e2d32d52f54c2f6521653c3e94da2493f3c5add
SRIKUMARAN R <68471103+01spidey@users.noreply.github.com>
2023-02-20 10:37:49 +0530

    Create Sort an Array.java

---
+++ Google/Sort an Array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int  i : nums) {
+            pq.offer(i);
+        }
+
+        int i = 0;
+        while (!pq.isEmpty()) {
+            nums[i++] = pq.poll();
+        }
+
+        return nums;
+    }
+}
